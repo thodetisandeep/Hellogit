54286dfff31b4b6688b0234557af3e307c1973ab
sandeep <tsandy011@gmail.com>
2017-10-28 16:11:18 -0700

    Committed AdamOnDemand project

---
+++ AdamOnDemand/src/Login/MedicalCourses.java
@@ -0,0 +1,23 @@
+package Login;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.annotations.Test;
+
+public class MedicalCourses extends SignUp {
+
+	@Test
+	public void MCourse() throws InterruptedException 
+	{
+		System.out.println("hi");
+		Thread.sleep(3000);
+	
+		//JavascriptExecutor jse = (JavascriptExecutor)driver;
+		//jse.executeScript("scroll(0, 1000);");
+		//Thread.sleep(2000);
+		//driver.findElement(By.linkText("Understanding the Physiology of the Skin")).click();
+		//System.out.println("hiieii");
+	
+	}	
+	
+	
+}
